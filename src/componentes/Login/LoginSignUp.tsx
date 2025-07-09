13178836af807917c65ae9c070c2927160c6e5db
JeffS1lva <jeffdsilva.29@gmail.com>
2025-07-09 11:27:29 -0300

    altered_api

--- src/componentes/Login/LoginSignUp.tsx
+++ src/componentes/Login/LoginSignUp.tsx
@@ -28,7 +28,7 @@ export function LoginSignupScreen() {
   });
 
   // URL da sua API - ajuste conforme necessário
-  const API_URL = 'https://video-class-backend-production.up.railway.app';
+  const API_URL = 'https://video-class-backend-production.up.railway.app/api';
 
   const handleInputChange = (e: { target: { name: any; value: any; }; }) => {
     setFormData({
